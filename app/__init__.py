2bc6a6e0eefc7882fea0bcceeb86c22949e2f284
Kyle Henry <kylehenry003@gmail.com>
2021-03-07 19:51:07 -0400

    Update login route

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, url_for
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
+from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:123@localhost/lab5"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 # Flask-Login login manager
